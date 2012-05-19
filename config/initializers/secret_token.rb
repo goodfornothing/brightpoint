f6aacb445b69f880559ee7d493aa3633d12e45eb
Alex Barlow <alexbarlowis@gmail.com>
2012-05-19 12:00:37 +0100

    Blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brightpoint::Application.config.secret_token = '0584c108768571558aae818357510ffbeb96814fa615ed6dff08fbe30b64f9fdd117d69bbdbda8dffc5c48a163d477ccd42fbe2a7a4956da8aedfc39d6acf957'
