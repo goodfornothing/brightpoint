8897eaf506c9d427117f71213bd42ffc94d93115
Alex Barlow <alexbarlowis@gmail.com>
2012-05-19 14:06:57 +0100

    Add defaut limit option

--- app/controllers/api/v1/data_sets_controller.rb
+++ app/controllers/api/v1/data_sets_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::DataSetsController < ApplicationController
   respond_to :json
   
   def index
-    @data_set = DataSet.where(['start_point >= ?', params[:start]]).limit(params[:size])
+    @data_set = DataSet.where(['start_point >= ?', params[:start]]).limit(params[:size] || 100)
     respond_with @data_set
   end
   
